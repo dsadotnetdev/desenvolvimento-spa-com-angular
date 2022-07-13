41166876a0780fc294b938b499e85f335ec3609e
Daniel Sena <dsawebdev@hotmail.com>
2022-07-13 08:00:36 -0300

    Adiciona exemplo da aula "Modules e Decorators"

---
+++ MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Seus componentes
+  ],
+  imports: [
+    CommonModule
+    // Seus módulos
+  ],
+  providers: [
+    // Seus serviços
+  ]
+})
+export class FuncionalidadeModule { }
